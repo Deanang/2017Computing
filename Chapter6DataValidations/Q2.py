ef18b33483293fedb93c041079e95595a4137032
Deanang <ngeekeng@gmail.com>
2017-07-17 20:59:20 +0800

    Changes up to Q6
    Changes up to Q6

--- Chapter6DataValidations/Q2.py
+++ Chapter6DataValidations/Q2.py
@@ -1,8 +1,5 @@
 is_valid = False
 while not is_valid:
-    input_str = input("Enter a phone number of the format xxxx-xxxx: ")
+    input_str = input('Enter a phone number: ')
     is_valid = len(input_str) == 9 and input_str[:4].isnumeric() \
-        and input_str[4] == '-' and input_str[5:].isnumeric()
-    if not is_valid:
-        print('The input is not valid!')
-print('Thank you for your input.')
+    and input_str[4] == '-' and input_str[5:].isnumeric()
--- Chapter6DataValidations/Q6.py
+++ Chapter6DataValidations/Q6.py
@@ -1,1 +1,1 @@
-message = input("Enter a message: ")punctuation_count = 0for character in message:    if not (character.isalnum() or character.isspace()):        punctuation_count += 1print(punctuation_count)
+message = input("Enter a message: ")punctuation_count = 0for character in message:    if not character.isalnum() and not character.isspace()):        punctuation_count += 1print(punctuation_count)
