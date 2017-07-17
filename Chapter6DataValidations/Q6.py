5d0568e4eaa0c87ed9ea5eacfe1c6d042d138fd6
Deanang <ngeekeng@gmail.com>
2017-07-17 21:14:42 +0800

    All questions updated
    All questions updated

--- Chapter6DataValidations/Q6.py
+++ Chapter6DataValidations/Q6.py
@@ -1,1 +1,6 @@
-message = input("Enter a message: ")punctuation_count = 0for character in message:    if not character.isalnum() and not character.isspace()):        punctuation_count += 1print(punctuation_count)
+message = input("Enter a message: ")
+punctuation_count = 0
+for character in message:
+    if not character.isalnum() and not character.isspace()):
+        punctuation_count += 1
+print(punctuation_count)
---
+++ Chapter6DataValidations/Q8.py
@@ -0,0 +1,5 @@
+lines = int(input("Input integer between 1 and 50: "))
+part1 = 0
+part2 = 0
+for i in range(1, lines+1):
+    print('*'*i, end='\n')
